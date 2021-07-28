48798bda729108ba5344935c0a63acdd2528c74e
kbalakirev <kvbalakirev@gmail.com>
2021-07-28 22:57:59 +0300

    add obj allocator

---
+++ alloc/alloc.h
@@ -0,0 +1,115 @@
+#pragma once
+
+#include <types/types.h>
+
+#include <cstdlib>
+#include <cassert>
+#include <utility>
+#include <new>
+
+namespace NAlloc {
+
+struct TBaseAlloc {
+    void* Alloc(ui64 c) {
+        return std::malloc(c);
+    }
+    void DeAlloc(void* p) {
+        std::free(p);
+    }
+};
+
+template <ui32 Size, class TAlloc = TBaseAlloc>
+class TFixedAlloc {
+public:
+    TFixedAlloc() = default;
+
+    void Initialize(ui32 c) {
+        assert(!Chuck_);
+        Chuck_ = TAlloc().Alloc(c * Block);
+        Free_ = c;
+        Max_ = c;
+    }
+
+    void* Alloc(ui64 = 0) {
+        if (Free_ == 0) {
+            return TAlloc().Alloc(Size);
+        }
+        if (Inited_ < Max_) {
+            *(ui32*) Ptr(Inited_) = Inited_ + 1;
+            ++Inited_;
+        }
+        void* block = Ptr(Head_);
+        Head_ = *(ui32*) block;
+        --Free_;
+        return block;
+    }
+
+    void DeAlloc(void* ptr) {
+        if (!ptr) {
+            return;
+        }
+        if (ptr < Chuck_ || ptr >= Ptr(Max_)) {
+            TAlloc().DeAlloc(ptr);
+            return;
+        }
+        assert(((char*) ptr - (char*) Chuck_) % Block == 0);
+        *(ui32*) ptr = Head_;
+        Head_ = Idx(ptr);
+        ++Free_;
+    }
+
+    void Destroy() noexcept {
+        TAlloc().DeAlloc(Chuck_);
+        Chuck_ = nullptr;
+    }
+
+private:
+    static constexpr ui32 ALIGN = sizeof(void*);
+    static constexpr ui32 Align(ui32 c) {
+        if (c % ALIGN) {
+            return c - (c % ALIGN) + ALIGN;
+        }
+        return c;
+    }
+    static constexpr ui64 Block = Align(Size);
+    ui32 Idx(void* p) const {
+        return ((char*) p - (char*) Chuck_) / Block;
+    }
+    void* Ptr(ui32 i) {
+        return (char*) Chuck_ + (Block * i);
+    }
+
+private:
+    void* Chuck_ = nullptr;
+    ui32 Head_ = 0;
+    ui32 Free_ = 0;
+    ui32 Inited_ = 0;
+    ui32 Max_ = 0;
+};
+
+template <class TObj, class TAlloc = TBaseAlloc>
+class TObjAlloc {
+public:
+    void Initialize(ui32 c) {
+        Alloc_.Initialize(c);
+    }
+
+    template <class... TArgs>
+    TObj* Alloc(TArgs... args) {
+        void* obj = Alloc_.Alloc();
+        return new (obj) TObj(std::forward<TArgs>(args)...);
+    }
+
+    void DeAlloc(void* ptr) {
+        Alloc_.DeAlloc(ptr);
+    }
+
+    void Destroy() {
+        Alloc_.Destroy();
+    }
+
+private:
+    TFixedAlloc<sizeof(TObj), TAlloc> Alloc_;
+};
+
+}
---
+++ alloc/example.cpp
@@ -0,0 +1,100 @@
+#include "alloc.h"
+
+#include <iostream>
+
+struct TIntrusiveNode {
+    TIntrusiveNode* Left = nullptr;
+    TIntrusiveNode* Right = nullptr;
+};
+
+template <class T, class TComparator>
+TIntrusiveNode* Add(TIntrusiveNode* node, T* value, TComparator comparator) {
+    if (!node) {
+        return value;
+    }
+
+    TIntrusiveNode* father = nullptr;
+    TIntrusiveNode* current = node;
+
+    bool left = false;
+
+    while (current) {
+        father = current;
+
+        if (comparator(*static_cast<T*>(current), *value)) {
+            current = current->Right;
+            left = false;
+        } else if (comparator(*value, *static_cast<T*>(current))) {
+            current = current->Left;
+            left = true;
+        } else {
+            return node;
+        }
+    }
+
+    if (left) {
+        father->Left = value;
+    } else {
+        father->Right = value;
+    }
+
+    return node;
+}
+
+template <class T, class TFunc>
+void ForEach(TIntrusiveNode* root, TFunc func) {
+    if (!root) {
+        return;
+    }
+
+    ForEach<T, TFunc>(root->Left, func);
+    func(*static_cast<T*>(root));
+    ForEach<T, TFunc>(root->Right, func);
+}
+
+template <class T, class TDeleter>
+void Free(TIntrusiveNode* root, TDeleter& deleter) {
+    if (!root) {
+        return;
+    }
+    Free<T>(static_cast<T*>(root->Left), deleter);
+    Free<T>(static_cast<T*>(root->Right), deleter);
+    deleter.DeAlloc(root);
+}
+
+struct TKey: public TIntrusiveNode {
+    explicit TKey(ui64 key)
+            : Key(key)
+    {
+    }
+
+    ui64 Key = 0;
+};
+
+void Example() {
+    constexpr ui32 N = 10;
+
+    NAlloc::TObjAlloc<TKey> alloc;
+    alloc.Initialize(N);
+
+    TIntrusiveNode* root = nullptr;
+
+    for (ui64 i = 0; i < N; ++i) {
+        auto* node = alloc.Alloc(N - i);
+        root = Add(root, node, [](const auto& lhs, const auto& rhs) {
+            return lhs.Key < rhs.Key;
+        });
+    }
+
+    ForEach<TKey>(root, [](const TKey& key) {
+       std::cout << key.Key << "\n";
+    });
+
+    Free<TKey>(root, alloc);
+}
+
+int main() {
+    Example();
+
+    return 0;
+}
